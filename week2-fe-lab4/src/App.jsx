8569aa8be1091013c21654e1dfa4ddabfa9d4c43
tamseelaa <tamseelm@metropolia.fi>
2025-10-29 01:00:43 +0200

    adding deleted files

---
+++ week2-fe-lab4/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header.jsx';
+import MainContent from './MainContent.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
